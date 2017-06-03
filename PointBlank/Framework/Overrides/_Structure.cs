d3e7f9f5cbea4ab25f6257f7def1a1499bb09443
AtiLion <thegrimpunisher1@gmail.com>
2017-06-03 09:33:55 +0200

    Added something to structures

--- PointBlank/Framework/Overrides/_Structure.cs
+++ PointBlank/Framework/Overrides/_Structure.cs
@@ -1,16 +1,19 @@
 ﻿using System;
+using System.Reflection;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using PointBlank.API.Detour;
+using SDG.Unturned;
 
 namespace PointBlank.Framework.Overrides
 {
-    internal class _Structure
+    internal static class _Structure
     {
-        public void askDamage(ushort amount)
+        [Detour(typeof(Structure), "askDamage", BindingFlags.Public | BindingFlags.Instance)]
+        public static void askDamage(this Structure stru, ushort amount)
         {
-
+            
         }
     }
 }
