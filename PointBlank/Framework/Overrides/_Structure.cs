8385f26aa098a48c755d55ccb4128661cc45048f
RoyIL <a4834833@gmail.com>
2017-06-03 10:32:53 +0300

    Update _Structure.cs

--- PointBlank/Framework/Overrides/_Structure.cs
+++ PointBlank/Framework/Overrides/_Structure.cs
@@ -10,9 +10,8 @@ namespace PointBlank.Framework.Overrides
 {
     internal class _Structure
     {
-        public void askDamage(Vector3 pos , float StructureDamage , float Radius)
+        public void askDamage(ushort amount)
         {
-           DamageTool.explode(pos, Radius, EDeathCause.ANIMAL, CSteamID.Nil, 0, 0, 0, 0, StructureDamage, 0, 0, 0, EExplosionDamageType.CONVENTIONAL, Radius, false);
         }
     }
 }
