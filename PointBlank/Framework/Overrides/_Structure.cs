3efd9b43e0c74b5241d8e2a6d5684274cf441f36
RoyIL <a4834833@gmail.com>
2017-06-03 10:26:40 +0300

    Update _Structure.cs

--- PointBlank/Framework/Overrides/_Structure.cs
+++ PointBlank/Framework/Overrides/_Structure.cs
@@ -3,14 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using PointBlank.API.Detour;
+using UnityEngine;
+using SDG.Unturned;
 
 namespace PointBlank.Framework.Overrides
 {
     internal class _Structure
     {
-        public void askDamage(ushort amount)
+        public void askDamage(Vector3 pos , float StructureDamage , float Radius)
         {
-
+           DamageTool.explode(pos, Radius, EDeathCause.ANIMAL, CSteamID.Nil, 0, 0, 0, 0, StructureDamage, 0, 0, 0, EExplosionDamageType.CONVENTIONAL, Radius, false);
         }
     }
 }
