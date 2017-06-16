c5414e963673b5151fc08764bdefb70f1f901aa8
AtiLion <thegrimpunisher1@gmail.com>
2017-06-16 15:57:04 +0200

    Added Parallel class for later

---
+++ PointBlank/API/Optimization/Parallel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PointBlank.API.Optimization
+{
+    /// <summary>
+    /// The .NET 3.5 implementation of parallel with optimization
+    /// </summary>
+    public static class Parallel
+    {
+
+    }
+}
