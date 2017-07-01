0c9519f85206092980e854b3d3b6eb34145dc9f6
Kr4ken-9 <kr4kenkt@gmail.com>
2017-07-01 07:33:39 -0400

    Fix

--- TestPlugin/Test.cs
+++ TestPlugin/Test.cs
@@ -1,14 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using System.Collections.Generic;
 using PointBlank.API;
 using PointBlank.API.Collections;
 using PointBlank.API.Plugins;
-using PointBlank.API.Unturned.Server;
-using PointBlank.API.Unturned.Structure;
-using SDG.Unturned;
-using Steamworks;
 
 namespace TestPlugin
 {
