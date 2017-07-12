da52bbdc03c8f2b8fcde35d2104dab4f47bce863
AtiLion <thegrimpunisher1@gmail.com>
2017-07-12 13:54:42 +0200

    Started work on launcher

---
+++ Launcher/Main.Designer.cs
@@ -0,0 +1,177 @@
+﻿namespace Launcher
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Main));
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabServers = new System.Windows.Forms.TabPage();
+            this.tabInformation = new System.Windows.Forms.TabPage();
+            this.tabConfiguration = new System.Windows.Forms.TabPage();
+            this.tabModules = new System.Windows.Forms.TabPage();
+            this.tabPlugins = new System.Windows.Forms.TabPage();
+            this.tabConsole = new System.Windows.Forms.TabPage();
+            this.tabPlayers = new System.Windows.Forms.TabPage();
+            this.tabSettings = new System.Windows.Forms.TabPage();
+            this.tabTranslation = new System.Windows.Forms.TabPage();
+            this.tabControl1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabServers);
+            this.tabControl1.Controls.Add(this.tabConsole);
+            this.tabControl1.Controls.Add(this.tabInformation);
+            this.tabControl1.Controls.Add(this.tabConfiguration);
+            this.tabControl1.Controls.Add(this.tabTranslation);
+            this.tabControl1.Controls.Add(this.tabModules);
+            this.tabControl1.Controls.Add(this.tabPlugins);
+            this.tabControl1.Controls.Add(this.tabPlayers);
+            this.tabControl1.Controls.Add(this.tabSettings);
+            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControl1.Location = new System.Drawing.Point(0, 0);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(641, 391);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabServers
+            // 
+            this.tabServers.Location = new System.Drawing.Point(4, 22);
+            this.tabServers.Name = "tabServers";
+            this.tabServers.Padding = new System.Windows.Forms.Padding(3);
+            this.tabServers.Size = new System.Drawing.Size(633, 365);
+            this.tabServers.TabIndex = 0;
+            this.tabServers.Text = "Servers";
+            this.tabServers.UseVisualStyleBackColor = true;
+            // 
+            // tabInformation
+            // 
+            this.tabInformation.Location = new System.Drawing.Point(4, 22);
+            this.tabInformation.Name = "tabInformation";
+            this.tabInformation.Size = new System.Drawing.Size(633, 365);
+            this.tabInformation.TabIndex = 1;
+            this.tabInformation.Text = "Information";
+            this.tabInformation.UseVisualStyleBackColor = true;
+            // 
+            // tabConfiguration
+            // 
+            this.tabConfiguration.Location = new System.Drawing.Point(4, 22);
+            this.tabConfiguration.Name = "tabConfiguration";
+            this.tabConfiguration.Size = new System.Drawing.Size(633, 365);
+            this.tabConfiguration.TabIndex = 2;
+            this.tabConfiguration.Text = "Configuration";
+            this.tabConfiguration.UseVisualStyleBackColor = true;
+            // 
+            // tabModules
+            // 
+            this.tabModules.Location = new System.Drawing.Point(4, 22);
+            this.tabModules.Name = "tabModules";
+            this.tabModules.Size = new System.Drawing.Size(633, 365);
+            this.tabModules.TabIndex = 3;
+            this.tabModules.Text = "Modules";
+            this.tabModules.UseVisualStyleBackColor = true;
+            // 
+            // tabPlugins
+            // 
+            this.tabPlugins.Location = new System.Drawing.Point(4, 22);
+            this.tabPlugins.Name = "tabPlugins";
+            this.tabPlugins.Size = new System.Drawing.Size(633, 365);
+            this.tabPlugins.TabIndex = 4;
+            this.tabPlugins.Text = "Plugins";
+            this.tabPlugins.UseVisualStyleBackColor = true;
+            // 
+            // tabConsole
+            // 
+            this.tabConsole.Location = new System.Drawing.Point(4, 22);
+            this.tabConsole.Name = "tabConsole";
+            this.tabConsole.Size = new System.Drawing.Size(633, 365);
+            this.tabConsole.TabIndex = 5;
+            this.tabConsole.Text = "Console";
+            this.tabConsole.UseVisualStyleBackColor = true;
+            // 
+            // tabPlayers
+            // 
+            this.tabPlayers.Location = new System.Drawing.Point(4, 22);
+            this.tabPlayers.Name = "tabPlayers";
+            this.tabPlayers.Size = new System.Drawing.Size(633, 365);
+            this.tabPlayers.TabIndex = 6;
+            this.tabPlayers.Text = "Players";
+            this.tabPlayers.UseVisualStyleBackColor = true;
+            // 
+            // tabSettings
+            // 
+            this.tabSettings.Location = new System.Drawing.Point(4, 22);
+            this.tabSettings.Name = "tabSettings";
+            this.tabSettings.Size = new System.Drawing.Size(633, 365);
+            this.tabSettings.TabIndex = 7;
+            this.tabSettings.Text = "Settings";
+            this.tabSettings.UseVisualStyleBackColor = true;
+            // 
+            // tabTranslation
+            // 
+            this.tabTranslation.Location = new System.Drawing.Point(4, 22);
+            this.tabTranslation.Name = "tabTranslation";
+            this.tabTranslation.Size = new System.Drawing.Size(633, 365);
+            this.tabTranslation.TabIndex = 8;
+            this.tabTranslation.Text = "Translation";
+            this.tabTranslation.UseVisualStyleBackColor = true;
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(641, 391);
+            this.Controls.Add(this.tabControl1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Main";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "PointBlank Launcher";
+            this.Load += new System.EventHandler(this.Main_Load);
+            this.tabControl1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabServers;
+        private System.Windows.Forms.TabPage tabInformation;
+        private System.Windows.Forms.TabPage tabConsole;
+        private System.Windows.Forms.TabPage tabConfiguration;
+        private System.Windows.Forms.TabPage tabTranslation;
+        private System.Windows.Forms.TabPage tabModules;
+        private System.Windows.Forms.TabPage tabPlugins;
+        private System.Windows.Forms.TabPage tabPlayers;
+        private System.Windows.Forms.TabPage tabSettings;
+    }
+}
+
