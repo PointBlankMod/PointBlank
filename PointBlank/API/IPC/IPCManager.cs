4e5294d719fdfe99b93912dde7227aea26d3140a
AtiLion <thegrimpunisher1@gmail.com>
2017-07-17 18:44:21 +0200

    Work on the IPC(untested)

---
+++ PointBlank/API/IPC/IPCManager.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using IPCM = PointBlank.Services.IPCManager.IPCManager;
+
+namespace PointBlank.API.IPC
+{
+    /// <summary>
+    /// Inter-process communication library
+    /// </summary>
+    public static class IPCManager
+    {
+        #region Properties
+        /// <summary>
+        /// List of all Inter-Process communication keys
+        /// </summary>
+        public static string[] IPCKeys => IPCM.IPC.Keys.ToArray();
+        #endregion
+
+        #region Functions
+        /// <summary>
+        /// Adds a key to the IPC
+        /// </summary>
+        /// <param name="key">The key to add</param>
+        /// <param name="value">The default value of the key</param>
+        public static void AddKey(string key, string value)
+        {
+            if (IPCM.IPC.ContainsKey(key))
+                return;
+
+            IPCM.IPC.Add(key, value);
+            IPCEvents.RunKeyAdded(key);
+        }
+        /// <summary>
+        /// Removes a key from the IPC
+        /// </summary>
+        /// <param name="key">The key to remove</param>
+        public static void RemoveKey(string key)
+        {
+            if (!IPCM.IPC.ContainsKey(key))
+                return;
+
+            IPCM.IPC.Remove(key);
+            IPCEvents.RunKeyRemoved(key);
+        }
+
+        /// <summary>
+        /// Changes a key's value
+        /// </summary>
+        /// <param name="key">The key to change</param>
+        /// <param name="value">The new value of the key</param>
+        public static void SetValue(string key, string value)
+        {
+            if (!IPCM.IPC.ContainsKey(key))
+                return;
+
+            IPCM.IPC[key] = value;
+            IPCEvents.RunKeyValueChanged(key, value);
+        }
+        /// <summary>
+        /// Gets the value of an IPC key
+        /// </summary>
+        /// <param name="key">The key to get the value from</param>
+        public static string GetValue(string key)
+        {
+            if (!IPCM.IPC.ContainsKey(key))
+                return null;
+
+            return IPCM.IPC[key];
+        }
+        #endregion
+    }
+}
---
+++ PointBlank/Services/IPCManager/IPCManager.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.IO;
+using System.Threading;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using PointBlank.API.Services;
+using PointBlank.API.IPC;
+
+namespace PointBlank.Services.IPCManager
+{
+    [Service("IPCManager", true)]
+    internal class IPCManager : Service
+    {
+        #region Info
+        public static readonly string FileLocation = Directory.GetCurrentDirectory() + "/IPC.ipc";
+        #endregion
+
+        #region Variables
+        private bool _Update = false;
+        private bool _Running = true;
+        private Thread _FileUpdaterThread;
+        #endregion
+
+        #region Properties
+        public static Dictionary<string, string> IPC { get; } = new Dictionary<string, string>();
+        #endregion
+
+        #region Override Functions
+        public override void Load()
+        {
+            if (File.Exists(FileLocation))
+                File.Delete(FileLocation);
+
+            // Setup the thread
+            _FileUpdaterThread = new Thread(new ThreadStart(UpdateFile));
+            _FileUpdaterThread.Start();
+
+            // Setup the events
+            IPCEvents.OnKeyValueChanged += new IPCEvents.KeyValueChangedHandler(OnKeySet);
+            IPCEvents.OnKeyRemoved += new IPCEvents.KeyListChangedHandler(OnKeyUpdated);
+            IPCEvents.OnKeyAdded += new IPCEvents.KeyListChangedHandler(OnKeyUpdated);
+        }
+
+        public override void Unload()
+        {
+            _Running = false;
+            _FileUpdaterThread.Abort();
+            File.Delete(FileLocation);
+        }
+        #endregion
+
+        #region Private Functions
+        private void UpdateFile()
+        {
+            while(_Running)
+            {
+                if (!_Update)
+                    continue;
+                List<string> contents = new List<string>();
+
+                foreach (string key in IPC.Keys)
+                    contents.Add(key + ":" + IPC[key]);
+
+                try
+                {
+                    File.WriteAllLines(FileLocation, contents.ToArray());
+                    _Update = false;
+                }
+#pragma warning disable CS0168 // Variable is declared but never used
+                catch (Exception ex)
+#pragma warning restore CS0168 // Variable is declared but never used
+                {
+                    Thread.Sleep(1);
+                }
+            }
+        }
+        #endregion
+
+        #region Event Functions
+        private void OnKeySet(string key, string value) => _Update = true;
+
+        private void OnKeyUpdated(string key) => _Update = true;
+        #endregion
+    }
+}
