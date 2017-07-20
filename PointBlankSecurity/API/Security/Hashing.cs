87f377e712ae1142ffd35feacba25bc69f41bc35
Kr4ken-9 <Kr4kenKT@gmail.com>
2017-07-20 12:08:10 -0500

    Security Notices

--- PointBlankSecurity/API/Security/Hashing.cs
+++ PointBlankSecurity/API/Security/Hashing.cs
@@ -12,17 +12,20 @@ namespace PointBlank.API.Security
     public static class Hashing
     {
         #region MD5
+        
         /// <summary>
         /// Calculate the MD5 hash from a string and return the hash in string format
         /// </summary>
         /// <param name="input">The string to hash using MD5</param>
         /// <returns>The MD5 hash of the string</returns>
+        /// [Obsolete("MD5 is highly insecure and should be avoided wherever possible", false)]
         public static string CalculateMD5String(string input) => CalculateMD5String(Encoding.UTF8.GetBytes(input));
         /// <summary>
         /// Calculate the MD5 hash from a byte array and return the hash in string format
         /// </summary>
         /// <param name="bytes">The byte array to hash using MD5</param>
         /// <returns>The MD5 hash of the byte array</returns>
+        [Obsolete("MD5 is highly insecure and should be avoided wherever possible", false)]
         public static string CalculateMD5String(byte[] bytes) => BitConverter.ToString(CalculateMD5Bytes(bytes)).Replace("-", string.Empty).ToLower();
 
         /// <summary>
@@ -30,20 +33,26 @@ namespace PointBlank.API.Security
         /// </summary>
         /// <param name="input">The string to hash using MD5</param>
         /// <returns>The MD5 hash of the string</returns>
+        [Obsolete("MD5 is highly insecure and should be avoided wherever possible", false)]
         public static byte[] CalculateMD5Bytes(string input) => CalculateMD5Bytes(Encoding.UTF8.GetBytes(input));
         /// <summary>
         /// Calculate the MD5 hash from a byte array and return the hash in byte array format
         /// </summary>
         /// <param name="input">The byte array to hash using MD5</param>
         /// <returns>The MD5 hash of the byte array</returns>
+        [Obsolete("MD5 is highly insecure and should be avoided wherever possible", false)]
         public static byte[] CalculateMD5Bytes(byte[] bytes) => ((HashAlgorithm)CryptoConfig.CreateFromName("MD5")).ComputeHash(bytes);
         #endregion
 
         #region SHA1
+        [Obsolete("SHA1 is prone to collisions and should be avoided if possible", false)]
         public static string CalculateSHA1String(string input) => CalculateSHA1String(Encoding.UTF8.GetBytes(input));
+        [Obsolete("SHA1 is prone to collisions and should be avoided if possible", false)]
         public static string CalculateSHA1String(byte[] bytes) => string.Join("", CalculateSHA1Bytes(bytes).Select(a => a.ToString("x2")).ToArray());
 
+        [Obsolete("SHA1 is prone to collisions and should be avoided if possible", false)]
         public static byte[] CalculateSHA1Bytes(string input) => CalculateSHA1Bytes(Encoding.UTF8.GetBytes(input));
+        [Obsolete("SHA1 is prone to collisions and should be avoided if possible", false)]
         public static byte[] CalculateSHA1Bytes(byte[] bytes)
         {
             using(SHA1Managed sha1 = new SHA1Managed())
